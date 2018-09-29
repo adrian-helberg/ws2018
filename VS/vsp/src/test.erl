1832fc16dbd12e836a4099a9c8c06dbbe12dbd96
adrian-helberg <adrianbostelmann@gmx.de>
2018-09-29 13:12:21 +0200

    Initial vsp project with test file

---
+++ VS/vsp/src/test.erl
@@ -0,0 +1,10 @@
+%%%-------------------------------------------------------------------
+%%% @author Adrian Helberg
+%%%-------------------------------------------------------------------
+-module(test).
+-author("Main").
+
+%% API
+-export([test_initialize/0]).
+
+test_initialize() -> io:fwrite("Initialize Test").
