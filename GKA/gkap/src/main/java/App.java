48ac5409a5c3a92577f67eb664226e910db463d1
adrian-helberg <adrianbostelmann@gmx.de>
2018-10-24 09:03:58 +0200

    GKAP clean-up

--- GKA/gkap/src/main/java/App.java
+++ GKA/gkap/src/main/java/App.java
@@ -1,9 +1,9 @@
-import org.jgrapht.graph.AbstractBaseGraph;
-
+/**
+ * Application
+ *
+ * @author Adrian Helberg
+ */
 public class App {
 
-    public static void main(String args[]) {
-        AbstractBaseGraph graph = utils.importGraph("graph01");
-        utils.exportGraph(graph, "test");
-    }
+    public static void main(String args[]) {}
 }
