a48859e02cca3a59bdc1d0f9be43529660930db7
adrian-helberg <adrianbostelmann@gmx.de>
2018-12-09 18:34:02 +0100

    Deleted project folder for task 5, moved to another repo

--- BW2/Aufgabe 4/Test.sql
+++ BW2/Aufgabe 4/Test.sql
@@ -1,9 +1,10 @@
 select 
     country, 
-    extract(year from "Time"), 
-    sum(salesquantity), 
-    sum(netsales) 
-from salesdata 
+    extract(year from "Time") AS year,
+    sum(salesquantity) AS quantity, 
+    sum(netsales) AS netsales
+from salesdata
 join customer on salesdata.customer_id = customer.id
-join product on salesdata.product_id = product.id 
-group by country, extract(year from "Time");
+join product on salesdata.product_id = product.id
+group by country, extract(year from "Time")
+order by netsales desc;
